f9c8a779356c4b55ba88a1f4e52586bd8b0936cb
arthur <arthurtquintao@gmail.com>
2022-04-25 15:33:47 +0200

    Move method implementation to FindASet class

--- app/src/main/java/be/kuleuven/findaset/model/AbstractFindASet.java
+++ app/src/main/java/be/kuleuven/findaset/model/AbstractFindASet.java
@@ -4,28 +4,15 @@ import be.kuleuven.findaset.model.card.AbstractCard;
 import be.kuleuven.findaset.test.TestableFindASet;
 
 public abstract class AbstractFindASet implements TestableFindASet {
-    @Override
-    public void setTable(AbstractCard[] table) {
 
-    }
+    public abstract void setTable(AbstractCard[] table);
 
-    @Override
-    public void updateTable(int[] cardIndexes) {
+    public abstract void updateTable(int[] cardIndexes);
 
-    }
+    public abstract AbstractCard getCard(int i);
 
-    @Override
-    public AbstractCard getCard(int i) {
-        return null;
-    }
+    public abstract void select(int i);
 
-    @Override
-    public void select(int i) {
 
-    }
-
-    @Override
-    public void unselect(int i) {
-
-    }
+    public abstract void unselect(int i);
 }
--- app/src/main/java/be/kuleuven/findaset/model/FindASet.java
+++ app/src/main/java/be/kuleuven/findaset/model/FindASet.java
@@ -1,6 +1,33 @@
 package be.kuleuven.findaset.model;
 
+import be.kuleuven.findaset.model.card.AbstractCard;
+
 public class FindASet extends AbstractFindASet{
     public FindASet() {
+    }
+
+    @Override
+    public void setTable(AbstractCard[] table) {
+
+    }
+
+    @Override
+    public void updateTable(int[] cardIndexes) {
+
+    }
+
+    @Override
+    public AbstractCard getCard(int i) {
+        return null;
+    }
+
+    @Override
+    public void select(int i) {
+
+    }
+
+    @Override
+    public void unselect(int i) {
+
     }
 }
