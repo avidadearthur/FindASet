7886b3542e381485e5f2739ae801c1e3de32d09c
arthur <arthurtquintao@gmail.com>
2022-04-25 09:34:28 +0200

    Move MainActivity

--- app/src/main/java/be/kuleuven/findaset/activities/MainActivity.java
+++ app/src/main/java/be/kuleuven/findaset/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package be.kuleuven.findaset;
+package be.kuleuven.findaset.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import be.kuleuven.findaset.R;
 
 public class MainActivity extends AppCompatActivity {
 
